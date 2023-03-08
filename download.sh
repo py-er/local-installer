39f41b55dde5ca29584c83f7d653a95262348351
Jacob <87302129+py-er@users.noreply.github.com>
2023-03-08 20:12:07 +0100

    Fix

--- download.sh
+++ download.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+if [[ $1 == "--pip" ]]
+then
+    if [[ $2 == "" ]]
+    then
+        echo "--pip (optional) program (requierd)"
+        exit 1
+    fi
+fi
+
+if ! [[ $1 == "--pip" ]]
+then
+    if ! [[ $2 == "" ]]
+    then
+        echo "--pip (optional) program (requierd)"
+        exit 1
+    fi
+fi
+
 if [[ $1 == "" ]]
 then
-    echo "Program"
+    echo "--pip (optional) program (requierd)"
     exit 1
 fi
+
+sudo echo > /dev/null
 
 repeatChar() {
     local input="$1"
@@ -14,23 +34,40 @@ repeatChar() {
 }
 
 fail=false
+if ! [[ $2 == "" ]]
+then
+program=$2
+PM="pip"
+else
 program=$1
+fi
 
-which apt 2>/dev/null | grep /apt &>/dev/null && PM="apt"
-which pacman 2>/dev/null | grep /pacman &>/dev/null && PM="pacman"
-[[ $PM == "" ]] && echo No && exit 0
+no_internet=false
+ping 8.8.8.8 -c 1 > /dev/null || no_internet=true
+$no_internet && echo "Failed to download program $program, no internet connection!" && exit 0
 
 
-[[ $PM == "apt" ]] && dependencies=$(apt-cache depends $program | grep -vE "Recommends|Suggests|Breaks|Conflicts|Depends: <" | sed 's/.*Depends: //' | sed 's/\ *//')
-[[ $PM == "pacman" ]] && dependencies=$(sudo pacman -Qi $program | grep "Depends On" | sed 's/.*: //' | perl -pe 's/ +/\n/g' && echo $program)
-[[ $dependencies == "" ]] && fail=true
+! [[ $PM == "pip" ]] && which apt 2>/dev/null | grep /apt &>/dev/null && PM="apt"
+! [[ $PM == "pip" ]] && which pacman 2>/dev/null | grep /pacman &>/dev/null && PM="pacman"
+[[ $PM == "" ]] && echo "Error: Couldn't recognize package manager..." && exit 0
+
 
 mkdir -p $program-install
 cd $program-install
+deldir=false
+[[ $(ls) == "" ]] && deldir=true
+
+
+[[ $PM == "apt" ]] && apt-cache depends $program 2>/dev/null | grep -vE "Recommends|Suggests|Breaks|Conflicts|Depends: <" | sed 's/.*Depends: //' | sed 's/\ *//' > dependencies
+[[ $PM == "pacman" ]] && sudo pacman -Qi $program 2>/dev/null | grep "Depends On" | sed 's/.*: //' | perl -pe 's/ +/\n/g' > dependencies
+! [[ $PM == "pip" ]] && [[ $(cat dependencies) == "" ]] && fail=true
+$fail && cd .. && rm -rf $program-install && echo "Faild to download program $program, program was not found!" &&  exit 1
+[[ $PM == "pacman" ]] && echo $program >> dependencies
+
 
 if [[ $PM == "apt" ]]
 then
-    packages=$(echo $dependencies | wc -l)
+    packages=$(cat dependencies | wc -l)
     po=30
     fo=0
 
@@ -40,7 +77,7 @@ then
     str=$(repeatChar "=" $big)$(repeatChar "-" $small )
     echo -ne "\r[$str] ($(($fo*100/$packages))%)"
 
-    for i in $dependencies
+    for i in $(cat dependencies)
     do
         apt download $i &>/dev/null || fail=true
         $fail && break
@@ -51,12 +88,24 @@ then
         str=$(repeatChar "=" $big)$(repeatChar "-" $small )
         echo -ne "\r[$str] ($(($fo*100/$packages))%)"
     done
+
+    $fail && echo && echo "Error downloading $program..." && rm dependencies
+    $fail && $deldir && cd .. && rm -rf $program-install
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished downloading $program successfully!"
+    rm dependencies
+
+
 elif [[ $PM == "pacman" ]]
 then
     sudo mkdir -p /var/cache/pacman/pkg-tmp
     sudo mv /var/cache/pacman/pkg/* /var/cache/pacman/pkg-tmp
 
-    packages=$(echo $dependencies | wc -l)
+    packages=$(cat dependencies | wc -l)
     po=30
     fo=0
 
@@ -66,10 +115,10 @@ then
     str=$(repeatChar "=" $big)$(repeatChar "-" $small )
     echo -ne "\r[$str] ($(($fo*100/$packages))%)"
 
-    for i in $dependencies
+    for i in $(cat dependencies)
     do
-        sudo pacman -Sw --noconfirm $i &>/dev/null
-
+        sudo pacman -Sw --noconfirm $i &>/dev/null || fail=true
+        $fail && break
         fo=$(($fo+1))
 
         big=$(((($po*100/$packages)*$fo)/100))
@@ -78,11 +127,43 @@ then
         echo -ne "\r[$str] ($(($fo*100/$packages))%)"
 
     done
+
+    $fail && echo && echo "Error downloading $program..." && rm dependencies
+    $fail && $deldir && cd .. && rm -rf $program-install
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished downloading $program successfully!"
+    rm dependencies
+
     sudo mv /var/cache/pacman/pkg/* .
     sudo mv /var/cache/pacman/pkg-tmp/* /var/cache/pacman/pkg
     sudo rm -rf /var/cache/pacman/pkg-tmp
+elif [[ $PM == "pip" ]]
+then
+    pip --version > /dev/null || fail=true
+    $fail && echo "Pip not installed" && exit 1
+
+    po=30
+
+    echo "Downloading $program..."
+    str=$(repeatChar "-" $po)
+    echo -ne "[$str] (0%)"
+    pip download $program &>/dev/null || fail=true
+
+    $fail && echo && echo "Faild to download program $program, program was not found!"
+    $fail && $deldir && cd .. && rm -rf $program-install
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished downloading $program successfully!"
+
 fi
 
 cd - > /dev/null
 
-$fail && rm -rf $program-install
+$fail && $deldir && rm -rf $program-install
--- install.sh
+++ install.sh
@@ -1,33 +1,165 @@
 #!/bin/bash
 
+if [[ $1 == "--pip" ]]
+then
+    if [[ $2 == "" ]]
+    then
+        echo "--pip (optional) program (requierd)"
+        exit 1
+    fi
+fi
+
+if ! [[ $1 == "--pip" ]]
+then
+    if ! [[ $2 == "" ]]
+    then
+        echo "--pip (optional) program (requierd)"
+        exit 1
+    fi
+fi
+
 if [[ $1 == "" ]]
 then
-    echo "Give Program"
+    echo "--pip (optional) program (requierd)"
     exit 1
 fi
 
 sudo echo &>/dev/null
 
-program=$1
+repeatChar() {
+    local input="$1"
+    local count="$2"
+    printf -v myString '%*s' "$count"
+    printf '%s\n' "${myString// /$input}"
+}
 
-which apt 2>/dev/null | grep /apt &>/dev/null && PM="apt"
-which pacman 2>/dev/null | grep /pacman &>/dev/null && PM="pacman"
-[[ $PM == "" ]] && echo No && exit 0
+fail=false
+if ! [[ $2 == "" ]]
+then
+program=$2
+PM="pip"
+else
+program=$1
+fi
+
+! [[ $PM == "pip" ]] && which apt 2>/dev/null | grep /apt &>/dev/null && PM="apt"
+! [[ $PM == "pip" ]] && which pacman 2>/dev/null | grep /pacman &>/dev/null && PM="pacman"
+[[ $PM == "" ]] && echo "Error: Couldn't recognize package manager..." && exit 0
+
 
 exit=false
 ls $program-install &>/dev/null || exit=true
-$exit && echo "Program not install in the directory"
+$exit && echo "Error: Couldn't find packages to install..."
 $exit && exit 0
+
+
 
 if [[ $PM == "pacman" ]]
 then
+
+    packages=$(ls $program-install | grep -v .sig | wc -l)
+    po=30
+    fo=0
+
     echo "Installing $program..."
+    big=$(((($po*100/$packages)*$fo)/100))
+    small=$(($po-$big))
+    str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+    echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+
     for i in $(ls $program-install | grep -v .sig)
     do
-        sudo pacman -U --noconfirm $program-install/$i &>/dev/null
+        sudo pacman -U --noconfirm $program-install/$i &>/dev/null || fail=true
+        $fail && break
+
+        fo=$(($fo+1))
+
+        big=$(((($po*100/$packages)*$fo)/100))
+        small=$(($po-$big))
+        str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+        echo -ne "\r[$str] ($(($fo*100/$packages))%)"
     done
+
+    $fail && echo && echo "Error installing $program..."
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished installing $program successfully!"
+
 elif [[ $PM == "apt" ]]
 then
+
+    packages=$(ls $program-install | wc -l)
+    po=30
+    fo=0
+
     echo "Installing $program..."
-    sudo dpkg -i $program-install/* &>/dev/null
+    big=$(((($po*100/$packages)*$fo)/100))
+    small=$(($po-$big))
+    str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+    echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    for i in $(ls $program-install)
+    do
+
+        sudo dpkg -i $program-install/$i &>/dev/null || fail=true
+        $fail && break
+
+        fo=$(($fo+1))
+
+        big=$(((($po*100/$packages)*$fo)/100))
+        small=$(($po-$big))
+        str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+        echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    done
+
+    $fail && echo && echo "Error installing $program..."
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished installing $program successfully!"
+
+elif [[ $PM == "pip" ]]
+then
+
+    pip --version > /dev/null || fail=true
+    $fail && echo "Pip not installed" && exit 1
+
+    packages=$(ls $program-install | wc -l)
+    po=30
+    fo=0
+
+    echo "Installing $program..."
+    big=$(((($po*100/$packages)*$fo)/100))
+    small=$(($po-$big))
+    str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+    echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    for i in $(ls $program-install)
+    do
+
+        pip install $program-install/$i &>/dev/null || fail=true
+        $fail && break
+
+        fo=$(($fo+1))
+
+        big=$(((($po*100/$packages)*$fo)/100))
+        small=$(($po-$big))
+        str=$(repeatChar "=" $big)$(repeatChar "-" $small )
+        echo -ne "\r[$str] ($(($fo*100/$packages))%)"
+
+    done
+    $fail && echo && echo "Error installing $program..."
+    $fail && exit 1
+
+    str=$(repeatChar "=" $po)
+    echo -ne "\r[$str] (100%)"
+    echo
+    echo "Finished installing $program successfully!"
 fi
